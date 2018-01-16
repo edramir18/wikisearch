7c437966f98477998c69722f7268be5d36918ebc
Eduardo Ramirez <edramir18@gmail.com>
2018-01-16 12:01:57 -0400

    Random fuction working and main section updated

--- README.md
+++ README.md
@@ -5,6 +5,20 @@ Repository to create a wikipedia search form, according the @freeCodeCamp challe
 This project will not use any framework or library to create the webpage, all the work will be done with plain html, css and javascript. No JQuery or Bootstrap will be used.
 
 ## Wikipedia API
+To be able to query the wikipedia is necesary to include the parameter *origin=\** on the url request:
+
+*Random Query*
+```
+https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=10&prop=info|extracts&inprop=url&exintro=true&explaintext=true
+```
+
+### Useful Links
+- [How to iterate over properties](http://2ality.com/2011/04/iterating-over-arrays-and-objects-in.html)
+- [How to use HTML Templates](https://www.w3schools.com/tags/tag_template.asp)
+- [Get element by Id](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
 
 ### BrowserSync
 The npm package **BrowerSync** will be used to livereload and automatic refresh of the project's files. More information about the installation and configuration can be found on the following link [BrowserSync](https://browsersync.io/)
+```
+br
+```
--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
+function loadJsonOnTemplate(template, pages) {
+    const article = document.getElementById(template.name);
+    const section = document.getElementById(template.target); 
+    
+    if(!article || !section) return;
+    
+    Object.keys(pages).forEach((k, i) => {
+        const cloneArticle = article.content.cloneNode(true);
+        template.properties.forEach((p) => {
+            cloneArticle.querySelector("." + p).textContent = pages[k][p];
+        });
+        if(section.children.length > i){
+            section.replaceChild(cloneArticle, section.children[i]);
+        } else {
+            section.appendChild(cloneArticle);
+        }
+    });
+
+}
+
 function searchWiki(){
     const searchInput = document.forms["formwiki"]["searchwiki"];
     searchInput.value = "";
     searchInput.blur();
 
-    const searchURL = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=10&format=json&origin=*";
+    const searchURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&grnlimit=10&prop=info|extracts&inprop=url&exintro=true&explaintext=true&exsentences=3&origin=*";
 
     const myHeaders = new Headers();
-
     const myInit = { method: 'GET',
                headers: myHeaders,
                mode: 'cors',
                cache: 'default' };
-    console.log(myHeaders);
     fetch(searchURL, myInit)
         .then( response => {
             if(!response.ok){
@@ -20,7 +38,11 @@ function searchWiki(){
             return response.json();
         })
         .then(json => {
-            console.log(json);
+            loadJsonOnTemplate({
+                name: "wikiarticle",
+                target: "wikisection",
+                properties: ["title", "extract"]
+            }, json.query.pages);
         })
         .catch(error => {
             console.log('There has been a problem with your fetch operation: ', error.message);
